ecbeb27222a586af0e3cfb3596c36c71dfababcc
dostonbek <74980968+dostonbek0033100@users.noreply.github.com>
2026-09-24 13:13:28 +0500

    Replace bot token and API key in bot_2.py
    Updated bot_2.py with new bot token and API key.

--- bot_2.py
+++ bot_2.py
@@ -8,13 +8,13 @@ from telethon.tl.functions.account import UpdateProfileRequest
 import google.generativeai as genai
 
 # ================= SOZLAMALAR =================
-BOT2_TOKEN = "YANGI_TOKENNI_SHU_YERGA_YOZING"
+BOT2_TOKEN = "8992607786:AAHoL2E8joe9KrPJyhP5UQXziSuVSi16lrM"
 API_ID = 946606
 API_HASH = "a183e9d1503a9c6514bd086dd03aeb8e"
 OWNER_ID = 1072547777
 
 # AI SOZLAMALARI
-GEMINI_API_KEY = "GEMINI_KALITNI_SHU_YERGA_YOZING"
+GEMINI_API_KEY = "AQ.Ab8RN6Jacg2QdiLuh78_vsrNRuY00lgxj6H_iiFgH99jfL5Ziw"
 genai.configure(api_key=GEMINI_API_KEY)
 ai_model = genai.GenerativeModel('gemini-1.5-flash')
 
